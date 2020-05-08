103bb9782be5bc7861195feb0e942ce81553446f
vicotrbb <victor.bona@hotmail.com>
2020-05-07 22:38:57 -0300

    Added some stuff, updated readme files, some pneumonia improvement

--- image_classification/pneumonia_detection/pneumonia_detection_implementation.py
+++ image_classification/pneumonia_detection/pneumonia_detection_implementation.py
@@ -8,44 +8,89 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 import pickle
-from keras.models import model_from_json
-from keras.models import load_model
+from tensorflow.keras.models import model_from_json
+from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
 
-# in this exact case, i have just two classes of data, but doing like this, i have an algorithm which can be used for more classes
-# in the future
-def prepare_data(datadir, img_size=28):
-	file_list = []
-	class_list = []
+class pneumonia:
 
-	train_dir_name = 'train'
-	test_dir_name = 'test'
+	def __init__(self):
+		self.categories = ['normal', 'pneumonia']
+		self.x = []
+		self.y = []
+		self.model = Sequential()
 
-	categories = ['normal', 'pneumonia']
-	data = []
-	for category in categories: 
-		path = os.path.join(datadir, category)
-		class_index = categories.index(category)
-		for img in os.listdir(path):
-		try :
-			img_array = cv2.imread(os.path.join(path, img), cv2.imread_grayscale) # parse the image to an array in greyscale
-			new_array = cv2.resize(img_array, (img_size, img_size)) # Resize the image to the correct size
-			data.append([new_array, class_num]) # Add it to the array
-		except exception as e:
-			print(f'Error processing {img}')
-			pass
+	# in this exact case, i have just two classes of data, but doing like this, i have an algorithm which can be used for 
+	# more classes in the future
+	def prepare_data(self, datadir, img_size=28):
+		file_list = []
+		class_list = []
 
-	random.shuffle(data) # randomiza a ordem dos arquivos
-	x = []
-	y = []
+		data = []
+		img_size = 28
 
-	X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+		for category in self.categories: 
+			path = os.path.join(datadir, category)
+			class_index = self.categories.index(category)
+			for img in os.listdir(path):
+				try:
+					img_array = cv2.imread(os.path.join(path, img), cv2.imread_grayscale) # parse the image to an array in greyscale
+					new_array = cv2.resize(img_array, (img_size, img_size)) # Resize the image to the correct size
+					data.append([new_array, class_num]) # Add it to the array
+				except:
+					print(f'Error processing {img}')
+					pass
 
-	# Creating the files containing all the information about your model
-	pickle_out = open("X.pickle", "wb")
-	pickle.dump(X, pickle_out)
-	pickle_out.close()
+		random.shuffle(data) # randomiza a ordem dos arquivos
 
-	pickle_out = open("y.pickle", "wb")
-	pickle.dump(y, pickle_out)
-	pickle_out.close()
+		for features, label in data:
+			self.x.append(features)
+			self.y.append(label)
+
+		self.x = np.array(x).reshape(-1, img_size, img_size, 1)
+		return self
+
+
+	def create_train_model(self):
+		x = self.x
+		y = self.y
+
+		x /= 255
+
+		# 3 convolutional layers
+		self.model.add(Conv2D(32, (3, 3), input_shape = x.shape[1:]))
+		self.model.add(Activation("relu"))
+		self.model.add(MaxPooling2D(pool_size=(2,2)))
+
+		self.model.add(Conv2D(64, (3, 3)))
+		self.model.add(Activation("relu"))
+		self.model.add(MaxPooling2D(pool_size=(2,2)))
+
+		self.model.add(Conv2D(64, (3, 3)))
+		self.model.add(Activation("relu"))
+		self.model.add(MaxPooling2D(pool_size=(2,2)))
+		self.model.add(Dropout(0.25))
+
+		# 2 hidden layers
+		self.model.add(Flatten())
+		self.model.add(Dense(128))
+		self.model.add(Activation("relu"))
+
+		self.model.add(Dense(128))
+		self.model.add(Activation("relu"))
+
+		# The output layer with 13 neurons, for 13 classes
+		self.model.add(Dense(13))
+		self.model.add(Activation("softmax"))
+
+		# Compile
+		self.model.compile(loss="sparse_categorical_crossentropy",
+				optimizer="adam",
+				metrics=["accuracy"])
+
+		self.model.fit(x, y, batch_size=32, epochs=40, validation_split=0.1)
+
+	def save_model(self):
+
+
+
