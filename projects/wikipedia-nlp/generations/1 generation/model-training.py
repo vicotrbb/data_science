f029cc91dbeb955a6361198f3ca081166f57acc3
Victor Bona <victor.bona@hotmail.com>
2020-08-27 23:36:56 -0300

    Updated first generation model from source project

--- projects/wikipedia-nlp/generations/1 generation/model-training.py
+++ projects/wikipedia-nlp/generations/1 generation/model-training.py
@@ -1,133 +1,156 @@
-import pickle
 import math
-import pandas as pd
 import numpy as np
-from numpy import array
-from pickle import load, save
 import string
 import json
 import os
 import sys
+import logging
+import boto3
+import nltk
+from dotenv import load_dotenv
+from nltk.corpus import stopwords
+from fastprogress.fastprogress import master_bar
 
-from sklearn.feature_extraction.text import CountVectorizer
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.preprocessing.sequence import pad_sequences
 from nltk.tokenize import word_tokenize
-
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import LSTM
 from tensorflow.keras.layers import Embedding
 
-from tensorflow.keras.models import load_model
-from tensorflow.keras.callbacks import ModelCheckpoint
-from keras.callbacks import EarlyStopping
-
-import nltk
 nltk.download('punkt')
 nltk.download('stopwords')
 
-chars = 0
+chars = ''
 maxlen = 60
 
-def setupLogger():
-	logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+def setup():
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    load_dotenv('.env')
+    global chars
+    global maxlen
+
+
+def shutdown(seconds=0, os='linux'):
+    if os == 'linux':
+        os.system(f'sudo shutdown -h -t sec {seconds}')
+    elif os == 'windows':
+        os.system(f'shutdown -s -t {seconds}')
+
+
+def downloadDataset():
+    s3 = boto3.client('s3')
+    bucket = os.getenv('S3_DATASET_BUCKET')
+    file = 'wikipedia-content-dataset.json'
+    s3.download_file(bucket, file, file)
+    logging.info(f'dataset downloaded')
 
 
 def prepareData(dataFile):
-	f = open(dataFile,)
-	data = json.load(f)
+    f = open(dataFile,)
+    data = json.load(f)
 
-	content = list(data[x] for x in data.keys())
-	text = ''
+    content = list(data[x] for x in data.keys())
+    text = ''
 
-	for c in content:
-		for i in c:
-	  	text += i
+    for c in content:
+        for i in c:
+            text += i
 
-	logging.info(f'Corpus length:', len(text))
+    logging.info(f'Corpus length: {len(text)}')
 
-	tokens = word_tokenize(text)
-	# convert to lower case
-	tokens = [w.lower() for w in tokens]
-	# remove punctuation from each word
-	table = str.maketrans('', '', string.punctuation)
-	stripped = [w.translate(table) for w in tokens]
-	# remove remaining tokens that are not alphabetic
-	words = [word for word in stripped if word.isalpha()]
-	# filter out stop words
-	from nltk.corpus import stopwords
-	stop_words = set(stopwords.words('english'))
-	words = [w for w in words if not w in stop_words]
+    tokens = word_tokenize(text)
+    tokens = [w.lower() for w in tokens]
+    table = str.maketrans('', '', string.punctuation)
+    stripped = [w.translate(table) for w in tokens]
+    words = [word for word in stripped if word.isalpha()]
+    stop_words = set(stopwords.words('english'))
+    words = [w for w in words if not w in stop_words]
 
-	text = ''
-	for c in words:
-	    text += c
-	    text += ' '
-	text = text.strip()
-	logging.info(f'Finished to load file')
-	return text
+    text = ''
+    for c in words:
+        text += c
+        text += ' '
+    text = text.strip()
+
+    logging.info(f'Finished to load file')
+    return text
 
 
 def prepareTrainingData(text):
-	step = 3
-	sentences = []
-	next_chars = []
+    step = 3
+    sentences = []
+    next_chars = []
 
-	for i in range(0, len(text) - maxlen, step):
-	    sentences.append(text[i: i + maxlen])
-	    next_chars.append(text[i + maxlen])
-	logging.info(f'Numero de sequencias:', len(sentences))
+    for i in range(0, len(text) - maxlen, step):
+        sentences.append(text[i: i + maxlen])
+        next_chars.append(text[i + maxlen])
+    logging.info(f'Number of sequences: {len(sentences)}')
 
-	chars = sorted(list(set(text)))
-	logging.info(f'Caracteres unicos:', len(chars))
-	char_indices = dict((char, chars.index(char)) for char in chars)
+    chars = sorted(list(set(text)))
+    logging.info(f'Unique characters: {len(chars)}')
+    char_indices = dict((char, chars.index(char)) for char in chars)
 
-	logging.info(f'Vetorizando o texto')
-	x = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
-	y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
-	for i, sentence in enumerate(sentences):
-	    for t, char in enumerate(sentence):
-	        x[i, t, char_indices[char]] = 1
-	    y[i, char_indices[next_chars[i]]] = 1
-	logging.info(f'Finished to prepare data')
-	return x, y
+    logging.info(f'Vectorizing text')
+    x = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
+    y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+    for i, sentence in enumerate(sentences):
+        for t, char in enumerate(sentence):
+            x[i, t, char_indices[char]] = 1
+        y[i, char_indices[next_chars[i]]] = 1
+
+    logging.info(f'Finished to prepare data')
+    return x, y
 
 
 def prepareTrainModel(x, y):
-	model = Sequential([
-    LSTM(len(chars), return_sequences=True, input_shape=(maxlen, len(chars))),
-    LSTM(len(chars), return_sequences=True),
-    LSTM(len(chars)),
-    Dense(len(chars), activation='relu'),
-    Dense(len(chars), activation='softmax')
-	])
-	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-	for epoch in range(1, 100):
-		logging.info(f'Epoch {epoch}')
-		model.fit(x, y, batch_size=128, epochs=1)
+    model = Sequential([
+        LSTM(len(chars), return_sequences=True,
+             input_shape=(maxlen, len(chars))),
+        LSTM(len(chars), return_sequences=True),
+        LSTM(len(chars)),
+        Dense(len(chars), activation='relu'),
+        Dropout(0.2),
+        Dense(len(chars), activation='softmax')
+    ])
 
-	logging.info(f'Finished to train model')
-	return model
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam', metrics=['accuracy'])
+
+    logging.info(f'Starting to train model')
+    mb = master_bar(range(1, 100))
+    for epoch in mb:
+        mb.comment = f'Epoch: {epoch}'
+        model.fit(x, y, batch_size=128, epochs=1)
+
+    logging.info(f'Finished to train model')
+    return model
 
 
 def saveModel(model):
-	s3 = boto3.client('s3')
-  file = 'wikipedia-nlp.hdf5'
-  gen = os.getenv('GENERATION')
-  bucket = os.getenv('S3_BUCKET')
+    logging.info(f'Saving model to S3')
+    s3 = boto3.client('s3')
+    file = 'wikipedia-nlp.hdf5'
+    gen = os.getenv('GENERATION')
+    bucket = os.getenv('S3_BUCKET')
 
-  model.save(file)
+    model.save(file)
 
-  s3.upload_file(file, bucket, gen+'/'+file)
-  return 0
+    s3.upload_file(file, bucket, gen+'/'+file)
+    return 0
 
 
-setupLogger()
-text = prepareData('wikipedia-content-dataset.json')
-x, y = prepareTrainingData(text)
-model = prepareTrainModel(x, y)
-saveModel(model)
-logging.info(f'Model training finished and file saved to s3.')
+def main():
+    setup()
+    downloadDataset()
+    text = prepareData('wikipedia-content-dataset.json')
+    x, y = prepareTrainingData(text)
+    model = prepareTrainModel(x, y)
+    saveModel(model)
+    logging.info(f'Model training finished and file saved to s3.')
+    shutdown()
+
+
+if __name__ == "__main__":
+    main()
